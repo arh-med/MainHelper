7fdca489fc9a7a4852690330591623a3e9b4fdbc
arh_med <arhhhemed@gmail.com>
2020-06-15 18:38:51 +0300

    Добавьте файлы проекта.

---
+++ MainHelper/Services/ScreenBehavior.cs
@@ -0,0 +1,31 @@
+﻿using MainHelper.WindowsProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace MainHelper.Services
+{
+    public class ScreenBehavior : Behavior<Image>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += ImageMouseLeftButtonDown;
+        }
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= ImageMouseLeftButtonDown;
+        }
+        private void ImageMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            HomePanel homePanel = new HomePanel();
+            homePanel.Show();
+            Application.Current.Windows[0].Close();
+        }
+    }
+}
---
+++ MainHelper/WindowsProject/HomePanel.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainHelper.WindowsProject
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePanel.xaml
+    /// </summary>
+    public partial class HomePanel : Window
+    {
+        public HomePanel()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
