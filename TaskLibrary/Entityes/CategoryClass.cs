2ad191dbd577638d52bf09ebaa5c393d51fe2e17
arh_med <arhhhemed@gmail.com>
2020-06-18 20:10:02 +0300

    Add MVVM Light

---
+++ TaskLibrary/Entityes/CategoryClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLibrary.Entityes
+{
+    public class CategoryClass
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ConsoleColor ColorCategory { get; set; }
+        public CategoryClass(string Name, ConsoleColor ColorCategory)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.ColorCategory = ColorCategory;
+        }
+    }
+}
---
+++ TaskLibrary/Entityes/TaskClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLibrary.Entityes
+{
+    public class TaskClass
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public DateTime DateTimeTask { get; set; }
+        public bool CheckTask { get; set; }
+        public DateTime AlarmTimeTask { get; set; }
+        public CategoryClass CategoryTask { get; set; }
+
+        public TaskClass(string Name, string Body, DateTime DateTimeTask,  DateTime AlarmTimeTask, CategoryClass CategoryTask)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.Body = Body;
+            this.DateTimeTask = DateTimeTask;
+            this.CheckTask = false;
+            this.AlarmTimeTask = AlarmTimeTask;
+            this.CategoryTask = CategoryTask;
+
+        }
+    }
+}
